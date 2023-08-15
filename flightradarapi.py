13dd2eb0e3e532bfe15d1b1f8572732a5ae782c4
Joris Falter <joris@jorisfalter.com>
2023-08-15 10:08:39 +0700

    x

--- flightradarapi.py
+++ flightradarapi.py
@@ -1,16 +1,23 @@
 from FlightRadar24 import FlightRadar24API
 
-def get_flight_data(flight_code):
+
+def get_flight_data():
     api = FlightRadar24API()
-    flight_data = api.get_flights(aircraft_type = "A388")
-    return flight_data
+    # flight_data = api.get_flights(aircraft_type="A388", details=True)
+    # return flight_data
+
+    for flight in api.get_flights(aircraft_type="A388"):
+        flight_details = api.get_flight_details(flight)
+        # print(flight.__dict__)
+        print(flight.number)
+        break
+
 
 if __name__ == "__main__":
-    flight_code = "EK397"  # Replace with the actual flight code you want to track
-    flight_data = get_flight_data(flight_code)
-    
-    if flight_data:
-        # Print or process the flight data
-        print(flight_data)
-    else:   
-        print("Failed to fetch data")
+    flight_data = get_flight_data()
+
+    # if flight_data:
+    #     # Print or process the flight data
+    #     print(flight_data.number)
+    # else:
+    #     print("Failed to fetch data")
