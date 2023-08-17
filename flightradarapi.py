cae59585552248e629d923334fe735318497dccd
Joris Falter <joris@jorisfalter.com>
2023-08-17 15:42:07 +0700

    x

--- flightradarapi.py
+++ flightradarapi.py
@@ -18,17 +18,24 @@ def get_flight_data():
         # print(json.dumps(flight_details,
         #       sort_keys=True, indent=4)[0:25000])
 
-        print(flight_details['time']['scheduled'])
-        print(time.localtime(flight_details['time']['scheduled']['departure']))
+        # print(flight_details['time']['scheduled'])
+        # print(time.localtime(flight_details['time']['scheduled']['departure']))
+        dep_time = time.localtime(flight_details['time']['scheduled']['departure'])
+        arr_time = time.localtime(flight_details['time']['scheduled']['arrival'])
 
         # print(flight.__dict__)
-        # print(flight.number, flight.origin_airport_iata,
-        #      flight.destination_airport_iata)
+        print(flight.number, flight.origin_airport_iata,
+             flight.destination_airport_iata,
+             dep_time.tm_year,"-",dep_time.tm_mon,"-",dep_time.tm_mday,dep_time.tm_hour,":",dep_time.tm_min,
+             arr_time.tm_year,"-",arr_time.tm_mon,"-",arr_time.tm_mday,arr_time.tm_hour,":",arr_time.tm_min)
         break
+
+        # it's converting to my time zone again aaaarggg
+        # goal: Number; dep airport, arr airport; dep time; arr time
 
 
 if __name__ == "__main__":
-    flight_data = get_flight_data()
+    flight_data=get_flight_data()
 
     # if flight_data:
     #     # Print or process the flight data
