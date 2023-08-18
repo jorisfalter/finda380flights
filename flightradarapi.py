7d1cedf5eacbc3873d543a12d45cea70bbb9627c
Joris Falter <joris@jorisfalter.com>
2023-08-18 12:06:59 +0700

    x

--- flightradarapi.py
+++ flightradarapi.py
@@ -20,60 +20,40 @@ def get_flight_data():
         # get all flight details
         # print(json.dumps(flight_details,
         #       sort_keys=True, indent=4)[0:25000])
-        
-        # print timezone details
 
-        print(flight_details['airport']['origin']['code'])
-        print(flight_details['airport']['origin']['timezone'])
-        print(flight_details['airport']['destination']['code'])
-        print(flight_details['airport']['destination']['timezone'])    
-        target_timezone_origin = flight_details['airport']['origin']['timezone']['name']
-        target_timezone_destination = flight_details['airport']['destination']['timezone']['name']
-        print(target_timezone_origin)
-        print(target_timezone_destination)
+        if flight_details['airport']['destination'] is not None:
+            target_timezone_origin = flight_details['airport']['origin']['timezone']['name']
+            target_timezone_destination = flight_details['airport']['destination']['timezone']['name']
 
-        # Unix timestamp
-        unix_dep_time = flight_details['time']['scheduled']['departure']
-        unix_arr_time = flight_details['time']['scheduled']['arrival']
+            # Unix timestamp
+            unix_dep_time = flight_details['time']['scheduled']['departure']
+            unix_arr_time = flight_details['time']['scheduled']['arrival']
 
-        # Convert Unix timestamp to datetime object
-        utc_dep_datetime = datetime.datetime.utcfromtimestamp(unix_dep_time)
-        utc_arr_datetime = datetime.datetime.utcfromtimestamp(unix_arr_time)
+            # Convert Unix timestamp to datetime object
+            utc_dep_datetime = datetime.datetime.utcfromtimestamp(unix_dep_time)
+            utc_arr_datetime = datetime.datetime.utcfromtimestamp(unix_arr_time)
 
+            # Set the UTC time zone to the datetime object
+            utc_dep_datetime = utc_dep_datetime.replace(tzinfo=pytz.utc)
+            utc_arr_datetime = utc_arr_datetime.replace(tzinfo=pytz.utc)
 
-        # Set the UTC time zone to the datetime object
-        utc_dep_datetime = utc_dep_datetime.replace(tzinfo=pytz.utc)
-        utc_arr_datetime = utc_arr_datetime.replace(tzinfo=pytz.utc)
+            # Convert to the target time zone
+            local_dep_datetime = utc_dep_datetime.astimezone(
+                pytz.timezone(target_timezone_origin))
+            local_arr_datetime = utc_arr_datetime.astimezone(
+                pytz.timezone(target_timezone_destination))
 
-        # Convert to the target time zone
-        local_dep_datetime = utc_dep_datetime.astimezone(pytz.timezone(target_timezone_origin))
-        local_arr_datetime = utc_arr_datetime.astimezone(pytz.timezone(target_timezone_destination))
+            # all details
+            # print(flight.__dict__)
 
-        print(local_dep_datetime)
-        print(local_arr_datetime)
-        print(flight.number)
-
-
-        # not sure what this was for
-        # print(flight_details['time']['scheduled'])
-        # print(time.localtime(flight_details['time']['scheduled']['departure']))
-      
-        # all details
-        # print(flight.__dict__)
-
-        # eventual code 
-
-        # dep_time = time.localtime(flight_details['time']['scheduled']['departure'])
-        # arr_time = time.localtime(flight_details['time']['scheduled']['arrival'])
-        # print(flight.number, flight.origin_airport_iata,
-        #      flight.destination_airport_iata,
-        #      dep_time.tm_year,"-",dep_time.tm_mon,"-",dep_time.tm_mday,dep_time.tm_hour,":",dep_time.tm_min,
-        #      arr_time.tm_year,"-",arr_time.tm_mon,"-",arr_time.tm_mday,arr_time.tm_hour,":",arr_time.tm_min)
-        break
-
-        # it's converting to my time zone again aaaarggg
-        # goal: Number; dep airport, arr airport; dep time; arr time
-
+            # data for databse
+            print(flight.number, flight.origin_airport_iata,
+                flight.destination_airport_iata, local_dep_datetime, local_arr_datetime)
+        else:
+            print("no destination")
+            # print(flight_details)
+       
+       # break
 
 if __name__ == "__main__":
     flight_data=get_flight_data()
