af568de5e1f601e9b940f70df5164072928dbd9a
Joris Falter <joris@jorisfalter.com>
2023-09-04 15:35:23 +0700

    x

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-var map = L.map("map").setView([51.505, -0.09], 13);
+//// This is the leaflet code
+// var map = L.map("map").setView([51.505, -0.09], 13);
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+// L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//   maxZoom: 19,
+//   attribution:
+//     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+// }).addTo(map);
+
+//// This is the mapbox code
+mapboxgl.accessToken = ""; // from European Startups
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [10, 56],
+  zoom: 2.7,
+});
