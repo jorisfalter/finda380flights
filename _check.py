3c409afd1084357a1fedbf926c2a89780c2892ca
Joris Falter <joris@jorisfalter.com>
2026-05-13 09:32:01 +0200

    wip: time-bucket mongo check

--- _check.py
+++ _check.py
@@ -10,7 +10,12 @@ print('Total docs:', col.count_documents({}))
 last = list(col.find({}).sort('loggingTime', -1).limit(5))
 for d in last:
     print('Recent:', d.get('loggingTime'), d.get('flightNumber'), d.get('originIata'), '->', d.get('destinationIata'))
-since = datetime.datetime.utcnow() - datetime.timedelta(days=30)
-print('Inserted in last 30 days:', col.count_documents({'loggingTime': {'$gte': since}}))
+now = datetime.datetime.utcnow()
+for d in [1, 7, 14, 30, 60, 90, 180]:
+    since = now - datetime.timedelta(days=d)
+    print(f'Last {d}d:', col.count_documents({'loggingTime': {'$gte': since}}))
+last_before_today = list(col.find({'loggingTime': {'$lt': now.replace(hour=0,minute=0,second=0,microsecond=0)}}).sort('loggingTime', -1).limit(1))
+if last_before_today:
+    print('Last insert before today:', last_before_today[0]['loggingTime'])
 routes_col = c['a380flightsDb']['a380routesCollection']
 print('Routes collection docs:', routes_col.count_documents({}))
